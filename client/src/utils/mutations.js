4b32b8b666b80de5f9384b5175597f68cf213ac5
Karla Goo <karla_goo@yahoo.com>
2021-11-02 22:53:16 -0500

    completed queries.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!){
+        login(email:$email, password: $password) {
+            token
+            profile{
+                _id
+                name
+            }
+        }
+    };
+`
+
+export const ADD_USER = gql`
+`
+
+export const SAVE_BOOK = gql`
+`
+export const REMOVE_BOOK = gql`
+`;
